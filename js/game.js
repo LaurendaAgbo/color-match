3589f7d28fb84e557feca6049a2435d14b8f4fbd
Hakim Ouake <kimly02@gmail.com>
2022-09-22 08:43:02 +0100

    Added game class and link it to html.

---
+++ js/game.js
@@ -0,0 +1,49 @@
+class Game {
+  constructor () {
+
+  }
+
+  init () {
+    
+  }
+
+  showPopup () {
+  
+  }
+  
+  hidePopup () {
+  
+  }
+  
+  playMusic () {
+  
+  }
+  
+  stopMusic () {
+  
+  }
+  
+  generateLevel () {
+  
+  }
+  
+  showLevel () {
+  
+  }
+  
+  setClock () {
+  
+  }
+  
+  keepScore () {
+  
+  }
+  
+  onPaireClick () {
+  
+  }
+  
+  checkGameStatus () {
+  
+  }
+}
